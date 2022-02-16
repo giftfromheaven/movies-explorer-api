9bcb3d5bc235526668478ea18724fa385c80094e
Ilya Dykin <dddiii06@yandex.ru>
2022-02-17 01:54:35 +0300

    user model fix

--- controllers/users.js
+++ controllers/users.js
@@ -1,27 +1,27 @@
 const { NODE_ENV, JWT_SECRET } = process.env;
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
-const NotFoundError = require("../errors/not-found-error");
-const BadRequestError = require("../errors/bad-request-error");
-const ConflictError = require("../errors/conflict-error");
-const NotAuthError = require("../errors/not-auth-error");
+const NotFoundError = require('../errors/not-found-error');
+const BadRequestError = require('../errors/bad-request-error');
+const ConflictError = require('../errors/conflict-error');
+const NotAuthError = require('../errors/not-auth-error');
 
 const getUserId = (req, res, next) => {
   const id = req.user._id;
 
   User.findById(id)
-    .orFail(new NotFoundError("Пользователь по указанному id не найден"))
+    .orFail(new NotFoundError('Пользователь по указанному id не найден'))
     .then((user) => {
       res.send(user);
     })
     .catch((err) => {
-      if (err.name === "CastError") {
+      if (err.name === 'CastError') {
         next(
           new BadRequestError(
-            "Переданы некорректные данные для поиска пользователя"
-          )
+            'Переданы некорректные данные для поиска пользователя',
+          ),
         );
       } else {
         next(err);
@@ -32,13 +32,11 @@ const getUserId = (req, res, next) => {
 const createUser = (req, res, next) => {
   bcrypt
     .hash(req.body.password, 10)
-    .then((hash) =>
-      User.create({
-        email: req.body.email,
-        password: hash,
-        name: req.body.name,
-      })
-    )
+    .then((hash) => User.create({
+      email: req.body.email,
+      password: hash,
+      name: req.body.name,
+    }))
     .then((user) => {
       res.send({
         name: user.name,
@@ -47,14 +45,14 @@ const createUser = (req, res, next) => {
       });
     })
     .catch((err) => {
-      if (err.name === "ValidationError" || err.name === "CastError") {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
         next(
           new BadRequestError(
-            "Переданы некорректные данные при создании пользователя."
-          )
+            'Переданы некорректные данные при создании пользователя.',
+          ),
         );
       } else if (err.code === 11000) {
-        next(new ConflictError("Пользователь с таким email уже существует."));
+        next(new ConflictError('Пользователь с таким email уже существует.'));
       } else {
         next(err);
       }
@@ -70,21 +68,21 @@ const updateProfile = (req, res, next) => {
   User.findOneAndUpdate(
     { _id: id },
     { name: newName, email: newEmail },
-    { runValidators: true, new: true }
+    { runValidators: true, new: true },
   )
-    .orFail(new NotFoundError("Пользователь по указанному _id не найден"))
+    .orFail(new NotFoundError('Пользователь по указанному _id не найден'))
     .then((user) => {
       res.send(user);
     })
     .catch((err) => {
-      if (err.name === "ValidationError" || err.name === "CastError") {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
         next(
           new BadRequestError(
-            "Переданы некорректные данные при обновлении профиля пользователя"
-          )
+            'Переданы некорректные данные при обновлении профиля пользователя',
+          ),
         );
       } else if (err.code === 11000) {
-        next(new ConflictError("Пользователь с таким email уже существует"));
+        next(new ConflictError('Пользователь с таким email уже существует'));
       } else {
         next(err);
       }
@@ -95,33 +93,33 @@ const updateProfile = (req, res, next) => {
 const login = (req, res, next) => {
   const { email, password } = req.body;
   User.findOne({ email })
-    .select("+password")
-    .orFail(new Error("IncorrectEmail"))
+    .select('+password')
+    .orFail(new Error('IncorrectEmail'))
     .then((user) => {
       bcrypt.compare(password, user.password).then((matched) => {
         if (!matched) {
-          next(new NotAuthError("Указан некорректный Email или пароль"));
+          next(new NotAuthError('Указан некорректный Email или пароль'));
         } else {
           const token = jwt.sign(
             { _id: user._id },
-            NODE_ENV === "production" ? JWT_SECRET : "randomdata",
-            { expiresIn: "7d" }
+            NODE_ENV === 'production' ? JWT_SECRET : 'randomdata',
+            { expiresIn: '7d' },
           );
 
           res
-            .cookie("jwt", token, {
+            .cookie('jwt', token, {
               maxAge: 3600000 * 24 * 7,
               httpOnly: true,
-              sameSite: "None",
+              sameSite: 'None',
               secure: true,
             })
-            .send({ message: "Вы успешно авторизовались" });
+            .send({ message: 'Вы успешно авторизовались' });
         }
       });
     })
     .catch((err) => {
-      if (err.message === "IncorrectEmail") {
-        next(new NotAuthError("Указан некорректный Email или пароль"));
+      if (err.message === 'IncorrectEmail') {
+        next(new NotAuthError('Указан некорректный Email или пароль'));
       } else {
         next(err);
       }
@@ -131,11 +129,11 @@ const login = (req, res, next) => {
 const logout = (req, res) => {
   res
     .status(200)
-    .clearCookie("jwt", {
-      sameSite: "None",
+    .clearCookie('jwt', {
+      sameSite: 'None',
       secure: true,
     })
-    .send({ message: "Выход" });
+    .send({ message: 'Выход' });
 };
 
 module.exports = {
