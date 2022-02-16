c3a2e8c6dba6a4faa8bd93892e404a58d2863600
Ilya Dykin <dddiii06@yandex.ru>
2022-02-17 01:17:11 +0300

    cors fix

--- utils/const.js
+++ utils/const.js
@@ -4,6 +4,8 @@ const Ok201 = 201;
 const mongoAdress = 'mongodb://localhost:27017/moviesdb';
 const port = 3000;
 const allowedCors = [
+  'http://gfh.movie.nomoredomains.work',
+  'https://gfh.movie.nomoredomains.work',
   'https://localhost:3000',
   'http://localhost:3000',
   'localhost:3000',
