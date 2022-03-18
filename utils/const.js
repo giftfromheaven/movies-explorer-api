770d16689fed30f6ba1b2beb56cb5ee5f74e9e36
Ilya <72022227+giftfromheaven@users.noreply.github.com>
2022-03-18 23:22:29 +0300

    port fix

--- utils/const.js
+++ utils/const.js
@@ -1,5 +1,5 @@
 const mongoAdress = 'mongodb://localhost:27017/bitfilmsdb';
-const port = 3001;
+const port = 3000;
 const allowedCors = [
   'http://gfh.movie.nomoredomains.work',
   'https://gfh.movie.nomoredomains.work',
