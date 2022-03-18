a6ca8f47339bb49ce3abcf48cc688ac5d686e27d
Ilya <72022227+giftfromheaven@users.noreply.github.com>
2022-03-18 21:18:14 +0300

    fix

--- utils/const.js
+++ utils/const.js
@@ -1,12 +1,12 @@
-const mongoAdress = "mongodb://localhost:27017/bitfilmsdb";
+const mongoAdress = 'mongodb://localhost:27017/bitfilmsdb';
 const port = 3001;
 const allowedCors = [
-  "http://gfh.movie.nomoredomains.work",
-  "https://gfh.movie.nomoredomains.work",
-  "gfh.movie.nomoredomains.work",
-  "https://localhost:3000",
-  "http://localhost:3000",
-  "localhost:3000",
+  'http://gfh.movie.nomoredomains.work',
+  'https://gfh.movie.nomoredomains.work',
+  'gfh.movie.nomoredomains.work',
+  'https://localhost:3000',
+  'http://localhost:3000',
+  'localhost:3000',
 ];
 
 module.exports = { mongoAdress, port, allowedCors };
